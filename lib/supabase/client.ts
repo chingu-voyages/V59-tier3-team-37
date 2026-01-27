f6cb7aa8a0ee192c5e79509f95b50d5e0cb6fc6f
Gerwyn <gerwynsim@hotmail.com>
2026-01-27 11:30:46 +0800

    delete unwanted files (implied by fouad)

--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -5,7 +5,7 @@ export function createClient() {
     !process.env.NEXT_PUBLIC_SUPABASE_URL ||
     !process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY
   ) {
-    throw Error("env.something is required");
+    throw Error("Please checkout that the supabase envs are set");
   }
   return createBrowserClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL as string,
