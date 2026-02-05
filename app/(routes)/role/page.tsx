a518eae61696b97c9bc7f3551e518f4b90acec2e
Gerwyn1 <52874400+Gerwyn1@users.noreply.github.com>
2026-02-05 12:52:26 +0800

    Navigation + route protection / Add route guards (#31)

--- app/(routes)/role/page.tsx
+++ app/(routes)/role/page.tsx
@@ -1,4 +1,35 @@
-const Role = () => {
-  return <div>Role</div>;
-};
-export default Role;
+// app/role/page.tsx
+import { selectRole } from "./actions";
+
+export default function RolePage() {
+  return (
+    <div className="flex flex-col gap-4 p-10">
+      <h1>Select your Role</h1>
+      <form>
+        {/* Pass the role to the server action */}
+        <button
+          formAction={async () => {
+            "use server";
+            await selectRole("student");
+          }}
+          className="bg-red-300 p-2 rounded m-4"
+          type='button'
+
+        >
+          I am a Student
+        </button>
+        <br />
+        <button
+          formAction={async () => {
+            "use server";
+            await selectRole("teacher");
+          }}
+          className="bg-red-300 p-2 rounded m-4"
+          type='button'
+        >
+          I am a Teacher
+        </button>
+      </form>
+    </div>
+  );
+}
