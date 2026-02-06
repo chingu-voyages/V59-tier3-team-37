d7f11cf00c5aee697b6a6fd8f9742187c6b47ba8
skelly <59558863+skellynb@users.noreply.github.com>
2026-02-06 01:33:51 +0100

    Bisi branch (#39)
    * WIP: my current changes
    
    * chore: biome auto fixes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import Footer from "@/components/layout/Footer";
+import Header from "@/components/layout/Header";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -27,7 +29,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased flex min-h-screen flex-col`}
       >
-        <AppLayout>{children}</AppLayout>
+        <Header />
+        <main className="flex-1">{children}</main>{" "}
+        {/* make page take available space */}
+        <Footer />
       </body>
     </html>
   );
