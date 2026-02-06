b48862ba72f435d149f4cf855e88191b8e225602
Fouad Tabbara <fmtabbara@hotmail.co.uk>
2026-02-06 11:47:54 +0000

    fix breaking code

--- components/custom/theme/ThemeToggle.tsx
+++ components/custom/theme/ThemeToggle.tsx
@@ -1,13 +1,13 @@
-import { useSessionStore } from "@/store/useSessionStore";
-
 export default function ThemeToggle() {
-  const darkMode = useSessionStore((state) => state.darkMode);
-  const toggleDarkMode = useSessionStore((state) => state.toggleDarkMode);
+  const onToggleDarkMode = () => {
+    return undefined;
+  };
 
+  const darkMode = false;
   return (
     <button
       type="button"
-      onClick={toggleDarkMode}
+      onClick={onToggleDarkMode}
       className="px-4 py-2 rounded bg-gray-200 dark:bg-gray-800 text-black dark:text-white transition"
     >
       {darkMode ? "Light Mode" : "Dark Mode"}
