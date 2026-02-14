e0f447220b050c6acb1b3aa7aaf114fe652ff842
Fouad Tabbara <fmtabbara@hotmail.co.uk>
2026-02-14 16:20:49 +0000

    chore: reorder dependencies in package.json for clarity and add react-markdown

--- components/custom/Spinner.tsx
+++ components/custom/Spinner.tsx
@@ -1,6 +1,6 @@
 const Spinner = () => {
   return (
-    <div className="flex min-h-screen items-center justify-center">
+    <div className="flex  items-center justify-center">
       <div className="animate-spin w-10 h-10 border-4 border-gray-300 border-t-black rounded-full" />
     </div>
   );
