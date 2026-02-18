3b62a3ed925a7f7dcbdd725751cabc78303d3b65
Maheswari Kumar <ksrimahi@gmail.com>
2026-02-18 12:37:36 +0530

    fixed error

--- components/interview/InterviewFear.tsx
+++ components/interview/InterviewFear.tsx
@@ -4,7 +4,6 @@ export default function InterviewFears() {
   return (
     <section className="w-full py-24 bg-white">
       <div className="max-w-6xl mx-auto px-6 text-center">
-        
         {/* Heading */}
         <h2 className="text-4xl md:text-5xl font-bold mb-6">
           Interviews should <br /> not be that hard
@@ -12,24 +11,38 @@ export default function InterviewFears() {
 
         {/* Subtitle */}
         <p className="text-[#39393B] max-w-2xl mx-auto mb-14">
-          You apply, you get the call, but what happens<br /> inside the interview
-          room is a mystery.
+          You apply, you get the call, but what happens
+          <br /> inside the interview room is a mystery.
         </p>
 
         {/* Cards */}
         <div className="grid gap-6 md:grid-cols-3 ">
           <FearCard
-            title={<>Fear of  <br />the Unknown Question</>}
+            title={
+              <>
+                Fear of <br />
+                the Unknown Question
+              </>
+            }
             description="The paralyzing worry that you will be asked a curveball, or a highly technical scenario."
           />
 
           <FearCard
-            title={<>Fear of <br /> Inadequate Depth</>}
+            title={
+              <>
+                Fear of <br /> Inadequate Depth
+              </>
+            }
             description="The anxiety that their answers will lack the industry nuance the interviewer is secretly listening for."
           />
 
           <FearCard
-            title={<>Fear of  <br />Repetitive, Generic Prep</>}
+            title={
+              <>
+                Fear of <br />
+                Repetitive, Generic Prep
+              </>
+            }
             description='The frustration of wasting limited preparation time on the same "top 10 questions" found online.'
           />
         </div>
