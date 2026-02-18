793b0b488ba24fbb7ae07c1fa450a18f1e106e25
Maheswari Kumar <75934872+MaheswariKumar@users.noreply.github.com>
2026-02-18 16:31:35 +0530

    Created AuthModal file (#55)
    * fixed error
    
    * created AuthModal

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -2,8 +2,12 @@
 
 import Image from "next/image";
 import Link from "next/link";
+import AuthPage from "../AuthModal";
+import { useState } from "react";
 
 export default function Header() {
+  const [openAuthState, setOpenAuthState] = useState(false);
+
   return (
     <header className="border-b border-gray-200 bg-white dark:bg-black px-3.75 py-3.75 flex flex-col sm:flex-row sm:items-start sm:justify-between gap-4">
       {/* Logo / App Name */}
@@ -34,12 +38,18 @@ export default function Header() {
       <div className="shrink-0 relative">
         <div className="-inset-1 rounded-full bg-[#3F1CD4] opacity-30 blur-xl z-0 cursor-pointer"></div>
         <Link
-          href="/auth"
+          href="/"
+          onClick={() => setOpenAuthState(true)}
           className="px-6 py-2 bg-primary text-primary-foreground rounded-full text-sm hover:bg-primary-foreground hover:text-primary transition-colors"
         >
           Login Now
         </Link>
       </div>
+
+      <AuthPage
+        isOpen={openAuthState}
+        onClose={() => setOpenAuthState(false)}
+      />
     </header>
   );
 }
